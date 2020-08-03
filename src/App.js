ad2d5860dab849191593d41067a2691cf96107bf
Victor Panteleev <panteleevvitja32@gmail.com>
2020-08-03 15:17:50 +0300

    one

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Loader from "./Loader/Loader";
+import Table from "./Table/Table";
+import _ from "lodash";
+import DetailRowView from "./DetailRowView/DetailRowView";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    isLoading: true,
+    data: [],
+    sort: "asc",
+    sortField: "id",
+    row: null,
+  };
+
+  async componentDidMount() {
+    const response = await fetch(
+      `http://www.filltext.com/?rows=1000&id={number|1000}&firstName={firstName}&delay=3&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}`
+    );
+    const data = await response.json();
+    // console.log(data);
+    this.setState({
+      isLoading: false,
+      data: _.orderBy(data, this.state.sortField, this.state.sort),
+    });
+  }
+  onSort = (sortField) => {
+    console.log(sortField);
+    const cloneData = this.state.data.concat();
+    const sortType = this.state.sort === "asc" ? "desc" : "asc";
+    const orderedData = _.orderBy(cloneData, sortField, sortType);
+    this.setState({
+      data: orderedData,
+      sort: sortType,
+      sortField,
+    });
+  };
+  onRowSelect = (row) => {
+    this.setState({ row });
+  };
+
+  render() {
+    return (
+      <div className="container">
+        {this.state.isLoading ? (
+          <Loader />
+        ) : (
+          <Table
+            data={this.state.data}
+            onSort={this.onSort}
+            sort={this.state.sort}
+            sortField={this.state.sortField}
+            onRowSelect={this.onRowSelect}
+          />
+        )}
+        {this.state.row ? <DetailRowView person={this.state.row} /> : null}
+      </div>
+    );
+  }
 }
 
 export default App;
